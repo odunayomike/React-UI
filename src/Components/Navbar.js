4bcec0f543065822c03991d07bfb5b28918d6430
Odunayomike <micheal0olawale@gmail.com>
2023-08-16 21:08:52 +0100

    working on expertise page

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,7 +1,28 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
+import "../App.css";
+import Logo from "../Assets/logo.png";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav>
+      <div className="navbar">
+        <div className="logo">
+          <img src={Logo} alt="" />
+        </div>
+        <div className="nav-link">
+          <a href="">Home</a>
+          <a href="">About us</a>
+          <a href="">Portfolio</a>
+          <a href="">Services</a>
+        </div>
+        <div className="navbar-button">
+          <button className="btn btn-primary">Login</button>
+          <button className="btn">Register</button>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
